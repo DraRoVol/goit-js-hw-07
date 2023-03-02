584f4fe9c7009a239282ffd665dcd781c22eb541
DraRoVol <107344569+DraRoVol@users.noreply.github.com>
2023-03-02 19:16:22 +0200

    update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,7 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector(".gallery");
 
 // Create and render markup
 const markup = galleryItems
@@ -19,37 +19,43 @@ const markup = galleryItems
       </div>
     `;
   })
-  .join('');
+  .join("");
 
-gallery.insertAdjacentHTML('beforeend', markup);
+gallery.insertAdjacentHTML("beforeend", markup);
 
 // Open modal on image click
-gallery.addEventListener('click', (event) => {
+
+gallery.addEventListener("click", (event) => {
   event.preventDefault();
 
-  const { nodeName, dataset, alt, src } = event.target;
+  const { nodeName, dataset, alt } = event.target;
 
-  if (nodeName !== 'IMG') return;
+  if (nodeName !== "IMG") return;
 
-  const instance = basicLightbox.create(`
+  const instance = basicLightbox.create(
+    `
     <div class="modal">
       <img src="${dataset.source}" alt="${alt}" />
     </div>
-  `);
+  `,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", handleKeyDown);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", handleKeyDown);
+      },
+    }
+  );
+
+  // Close modal on ESC press
+
+  function handleKeyDown(event) {
+    if (event.code !== "Escape") return;
+    instance.close();
+  }
 
   instance.show();
 
-  // Close modal on ESC press
-  const handleKeyDown = (event) => {
-    if (event.code !== 'Escape') return;
-    instance.close();
-  };
-
-  document.addEventListener('keydown', handleKeyDown);
-
-  // Remove listener when modal is closed
-  instance.on('close', () => {
-    document.removeEventListener('keydown', handleKeyDown);
-  });
+  document.addEventListener("keydown", handleKeyDown);
 });
-
